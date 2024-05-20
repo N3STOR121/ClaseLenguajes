e03e315dc8ca330688d27f3023abddb72be0dc89
N3STOR121 <l21211920@tectijuana.edu.mx>
2024-05-20 16:35:06 -0600

    Update chat.py

--- Practica 5/Chat/chat.py
+++ Practica 5/Chat/chat.py
@@ -19,53 +19,14 @@ from adafruit_display_text import wrap_text_to_pixels
 from adafruit_display_text.bitmap_label import Label
 from adafruit_ticks import ticks_add, ticks_less, ticks_ms
 
-
-# Choose your own prompt and wait messages, either by changing it below inside
-# the """triple quoted""" string, or by putting it in your settings.toml file,
-# like so:
-#
-# MY_PROMPT="Give me an idea for a gluten free, keto dinner. Write one sentence"
-# PLEASE_WAIT="Cooking something up just for you"
-#
-# Experimentation is best to figure out what works. Usually you'll want to ask
-# for just one sentence or paragraph, since the 128x32 pixel screen can't hold
-# much text!
-
-# Here are some that the author found worked reasonably well:
-
-# Give me an idea for a plant-based dinner. Write one sentence
-#
-# Give jepler (they/them) a cliched and flowery description as a comic book
-# supervillain. write one sentence.
-#
-# Invent and describe an alien species. write one sentence
-#
-# Invent a zany 'as seen on' product that can't possibly work. One sentence
-#
-# Tell a 1-sentence story about a kitten and a funny mishap
-#
-# Make up a 1-sentence fortune for me
-#
-# In first person, write a 1-sentence story about an AI avoiding boredom in a creative way.
-#
-# Pick an everyday object (don't say what it is) and describe it using only the
-# ten hundred most common words.
-#
-# Invent an alien animal or plant, name it, and vividly describe it in 1
-# sentence
-#
-# Invent and vividly describe an alien species. write one paragraph
-
-prompt=os.getenv("MY_PROMPT", """
-Cuentame un dato curioso""").strip()
-please_wait=os.getenv("Por favor, Espera un momento", """
-Buscando....
-""").strip()
+# Customizing the prompts and wait messages
+prompt = os.getenv("MY_PROMPT", "Cuentame un dato curioso").strip()
+please_wait = os.getenv("PLEASE_WAIT", "Buscando....").strip()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 nice_font = load_font("helvR08.pcf")
-line_spacing = 9 # in pixels
+line_spacing = 9  # in pixels
 
 #  i2c display setup
 displayio.release_displays()
@@ -84,7 +45,9 @@ display = adafruit_displayio_ssd1306.SSD1306(
 )
 if openai_api_key is None:
     input("Place your\nOPENAI_API_KEY\nin settings.toml")
+
 display.auto_refresh = False
+
 
 class WrappedTextDisplay(displayio.Group):
     def __init__(self):
@@ -158,13 +121,15 @@ class WrappedTextDisplay(displayio.Group):
         # Actually update the display all at once
         display.refresh()
 
+
 display.root_group = wrapped_text = WrappedTextDisplay()
+
 
 def wait_button_scroll_text():
     led.switch_to_output(True)
     keys.events.clear()
     deadline = ticks_add(ticks_ms(),
-            5000 if wrapped_text.on_last_line() else 1000)
+                         5000 if wrapped_text.on_last_line() else 1000)
     while True:
         if (event := keys.events.get()) and event.pressed:
             break
@@ -172,13 +137,15 @@ def wait_button_scroll_text():
             wrapped_text.scroll_next_line()
             wrapped_text.refresh()
             deadline = ticks_add(deadline,
-                    5000 if wrapped_text.on_last_line() else 1000)
+                                 5000 if wrapped_text.on_last_line() else 1000)
     led.value = False
+
 
 if radio.ipv4_address is None:
     wrapped_text.show(f"Conectando con {os.getenv('WIFI_SSID')}")
     radio.connect(os.getenv('WIFI_SSID'), os.getenv('WIFI_PASSWORD'))
 requests = adafruit_requests.Session(socketpool.SocketPool(radio), ssl.create_default_context())
+
 
 def iter_lines(resp):
     partial_line = []
@@ -190,6 +157,7 @@ def iter_lines(resp):
             partial_line.append(c)
     if partial_line:
         yield (b"".join(partial_line)).decode('utf-8')
+
 
 full_prompt = [
     {"role": "user", "content": prompt},
@@ -204,11 +172,11 @@ try:
         wrapped_text.show(please_wait)
 
         with requests.post("https://api.openai.com/v1/chat/completions",
-            json={"model": "gpt-3.5-turbo", "messages": full_prompt, "stream": True},
-            headers={
-                "Authorization": f"Bearer {openai_api_key}",
-            },
-            ) as response:
+                           json={"model": "gpt-3.5-turbo", "messages": full_prompt, "stream": True},
+                           headers={
+                               "Authorization": f"Bearer {openai_api_key}",
+                           },
+                           ) as response:
 
             wrapped_text.set_text("")
             if response.status_code != 200:
@@ -229,8 +197,8 @@ try:
                         if token:
                             wrapped_text.add_show(token)
                 wait_button_scroll_text()
-except Exception as e: # pylint: disable=broad-except
-    traceback.print_exception(e) # pylint: disable=no-value-for-parameter
+except Exception as e:  # pylint: disable=broad-except
+    traceback.print_exception(e)  # pylint: disable=no-value-for-parameter
     print(end="\n\n\nAn error occurred\n\nPress button\nto reload")
     display.root_group = displayio.CIRCUITPYTHON_TERMINAL
     display.auto_refresh = True
