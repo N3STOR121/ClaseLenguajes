eb55872f1f8271e9ea54231627b34d8258dd59ec
N3STOR121 <l21211920@tectijuana.edu.mx>
2024-05-20 23:14:23 -0600

    Add files via upload

---
+++ Proyecto Final/ProyectoFinal/ProyectoFinal.ino
@@ -0,0 +1,30 @@
+
+//OV767X - Camera Capture Raw Bytes
+//Proyecto Final
+//Bonilla Castañeda Nestor Miguel
+
+#include <Arduino_OV767X.h>
+
+int bytesPerFrame;
+
+byte data[176 * 144 * 2]; // QVGA: 320x240 X 2 bytes per pixel (RGB565)
+
+void setup() {
+  Serial.begin(460800 ); 
+  while (!Serial);
+
+  if (!Camera.begin(QCIF, RGB565, 1)) {
+    Serial.println("Failed to initialize camera!");
+    while (1);
+  }
+
+  bytesPerFrame = Camera.width() * Camera.height() * Camera.bytesPerPixel();
+
+   Camera.testPattern();
+}
+
+void loop() {
+  Camera.readFrame(data);
+
+  Serial.write(data, bytesPerFrame);
+}
